86a1016f6b7cf502f66acb91085d357445339679
Anthony Risch <rischthefisch@gmail.com>
2020-02-06 23:46:33 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,20 +1,7 @@
 class Dog 
-  def name = (dog_name)
-    @name = dog_name
-end
-
-def name
-  @name
-end
- 
-def breed = (dog_breed)
-  @breed = dog_breed
-end
-
-def breed 
-  @breed 
-end
-end
+  
+  def name = (doggo_name)
+    @name = 
 
 fido = Dog.new 
 fido.name = "Fido"
